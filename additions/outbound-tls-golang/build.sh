c0c6c7a55663f3f5c99b9f19fefc8e39a31a9252
Alon Girmonsky <1990761+alongir@users.noreply.github.com>
2024-01-14 10:25:22 -0800

    TLS demo files

--- additions/outbound-tls-golang/build.sh
+++ additions/outbound-tls-golang/build.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-docker build . -t mertyildiran/mizutest-outbound-tls-golang:latest && docker push mertyildiran/mizutest-outbound-tls-golang:latest
+# docker build . -t mertyildiran/mizutest-outbound-tls-golang:latest && docker push mertyildiran/mizutest-outbound-tls-golang:latest
+docker buildx build --platform linux/amd64 -t alongir/mizutest-outbound-tls-openssl:latest . --push 
--- additions/outbound-tls-golang/main.go
+++ additions/outbound-tls-golang/main.go
@@ -32,19 +32,23 @@ func main() {
 		req, err := http.NewRequest("GET", "https://gorest.co.in/public/v2/not-found", nil)
 		if err != nil {
 			fmt.Println(err)
+			continue
 		}
 		req.Header.Set("cache-control", "no-cache")
 		req.Header.Set("x-powered-by", "golang")
 		res, err := client.Do(req)
 		if err != nil {
 			fmt.Println(err)
+			continue
 		}
-		// fmt.Printf("res: %+v\n", res)
+		defer res.Body.Close()
+		fmt.Printf("res: %+v\n", res)
 
 		payload := strings.NewReader(fmt.Sprintf("------WebKitFormBoundary7MA4YWxkTrZu0gW\r\nContent-Disposition: form-data; name=\"email\"\r\n\r\njohn.doe.%d@example.com\r\n------WebKitFormBoundary7MA4YWxkTrZu0gW\r\nContent-Disposition: form-data; name=\"name\"\r\n\r\nJohn\r\n------WebKitFormBoundary7MA4YWxkTrZu0gW\r\nContent-Disposition: form-data; name=\"gender\"\r\n\r\nmale\r\n------WebKitFormBoundary7MA4YWxkTrZu0gW\r\nContent-Disposition: form-data; name=\"status\"\r\n\r\nactive\r\n------WebKitFormBoundary7MA4YWxkTrZu0gW--", j))
 		req, err = http.NewRequest("POST", "https://gorest.co.in/public/v2/users", payload)
 		if err != nil {
 			fmt.Println(err)
+			continue
 		}
 		req.Header.Set("content-type", "multipart/form-data; boundary=----WebKitFormBoundary7MA4YWxkTrZu0gW")
 		req.Header.Set("authorization", fmt.Sprintf("Bearer %s", ACCESS_TOKEN))
@@ -53,37 +57,44 @@ func main() {
 		res, err = client.Do(req)
 		if err != nil {
 			fmt.Println(err)
+			continue
 		}
-		// fmt.Printf("res: %+v\n", res)
+		defer res.Body.Close()
+		fmt.Printf("res: %+v\n", res)
 
 		req, err = http.NewRequest("GET", "https://gorest.co.in/public/v2/users", nil)
 		if err != nil {
 			fmt.Println(err)
+			continue
 		}
 		req.Header.Set("cache-control", "no-cache")
 		req.Header.Set("x-powered-by", "golang")
 		res, err = client.Do(req)
 		if err != nil {
 			fmt.Println(err)
+			continue
 		}
-		// fmt.Printf("res: %+v\n", res)
+		defer res.Body.Close()
+		fmt.Printf("res: %+v\n", res)
 
 		req, err = http.NewRequest("GET", "https://gorest.co.in/public/v2/posts", nil)
 		if err != nil {
 			fmt.Println(err)
+			continue
 		}
 		req.Header.Set("cache-control", "no-cache")
 		req.Header.Set("x-powered-by", "golang")
 		res, err = client.Do(req)
 		if err != nil {
 			fmt.Println(err)
+			continue
 		}
-		// fmt.Printf("res: %+v\n", res)
+		defer res.Body.Close()
+		fmt.Printf("res: %+v\n", res)
 		_, err = io.ReadAll(res.Body)
 		if err != nil {
 			fmt.Println(err)
 		}
-		// fmt.Printf("body: %v\n", string(body))
 
 		time.Sleep(3 * time.Second)
 	}
